dc7f1a56e2dcefabfb0162eb1de47a4500e990dc
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-10-31 13:32:59 +0000

    feat: creation and insertion of InfoPill component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -10,10 +10,13 @@ export function Main({titlePage, subTitlePage}: MainProps) {
   return (
     <main>
       <img src={`https://github.com/${titlePage}.png`} alt={`${titlePage} profile image`} className='profile-img' />
+      <div className="infopill-container">
+        <InfoPill keyInfo='Followers' valueInfo='27839' />
+        <InfoPill keyInfo='Following' valueInfo='0' />
+        <InfoPill keyInfo='Location' valueInfo='San Francisco, CA' />
+      </div>
       <h1 className='title-page'>{titlePage}</h1>
       <p className='subtitle-page'>{subTitlePage}</p>
-
-      <InfoPill keyInfo='Followers' valueInfo='27839' />
     </main>
   )
 }
