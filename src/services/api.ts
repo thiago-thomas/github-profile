278eac43a67c5f4b82541632cb05308d95309a26
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-11-06 21:22:26 +0000

    wip: fetching data with pagination (4 latests repo by page)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,31 +1,32 @@
-const API_URL = 'https://api.github.com';
+const API_URL = "https://api.github.com";
+const per_page = 4;
 
 export async function fetchGithubReposByUser(user: string): Promise<any> {
   try {
-    const url = `${API_URL}/users/${user}/repos`
+    const url = `${API_URL}/users/${user}/repos?sort=updated&direction=desc&per_page=${per_page}`;
     const response = await fetch(url);
 
-    if(!response.ok) {
-      throw new Error(`Error on requesting repos: ${response.status}`)
+    if (!response.ok) {
+      throw new Error(`Error on requesting repos: ${response.status}`);
     }
-    
+
     return await response.json();
   } catch (error) {
-    throw new Error(`An error occurred: ${error}`)
+    throw new Error(`An error occurred: ${error}`);
   }
 }
 
 export async function fetchGithubUser(user: string): Promise<any> {
   try {
-    const url = `${API_URL}/users/${user}`
+    const url = `${API_URL}/users/${user}`;
     const response = await fetch(url);
-    
-    if(!response.ok) {
-      throw new Error(`Error on requesting user: ${response.status}`)
+
+    if (!response.ok) {
+      throw new Error(`Error on requesting user: ${response.status}`);
     }
-    
+
     return await response.json();
   } catch (error) {
-    throw new Error(`An error occurred: ${error}`)
+    throw new Error(`An error occurred: ${error}`);
   }
 }
