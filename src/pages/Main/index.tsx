338bd12ea890ef114f1aba93e4de00e6bb2253e5
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-11-06 22:37:10 +0000

    feat: view all repos button (limit: 30) done, and challenge done! 😜

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -12,7 +12,7 @@ type GithubProfile = {
   following: number;
   location: string;
   avatar_url: string;
-}
+};
 
 type GithubRepo = {
   name: string;
@@ -23,28 +23,44 @@ type GithubRepo = {
   forks_count: number;
   stargazers_count: number;
   updated_at: string;
-}
+};
 
 type MainProps = {
   githubUser: string;
-}
+};
 
-export function Main({githubUser}: MainProps) {
+export function Main({ githubUser }: MainProps) {
   const [githubProfile, setGithubProfile] = useState<GithubProfile>();
   const [githubRepos, setGithubRepos] = useState<GithubRepo[]>([]);
+  const [isShowingAllRepos, setIsShowingAllRepos] = useState<boolean>(false);
 
   useEffect(() => {
-    fetchGithubReposByUser(githubUser)
-      .then(repos => {
+    async function fetchData() {
+      try {
+        const [repos, user] = await Promise.all([
+          fetchGithubReposByUser(githubUser),
+          fetchGithubUser(githubUser),
+        ]);
         setGithubRepos(repos);
-      });
-
-    fetchGithubUser(githubUser)
-      .then(user => {
+        setIsShowingAllRepos(false);
         setGithubProfile(user);
-      });
+      } catch (err) {
+        console.error(err);
+      }
+    }
 
+    fetchData();
   }, [githubUser]);
+
+  async function viewAllRepos() {
+    try {
+      const repos = await fetchGithubReposByUser(githubUser, 30);
+      setGithubRepos(repos);
+      setIsShowingAllRepos(true);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <main>
@@ -54,27 +70,42 @@ export function Main({githubUser}: MainProps) {
         className="profile-img"
       />
       <div className="infopill-container">
-        <InfoPill keyInfo="Followers" valueInfo={githubProfile?.followers?.toString()!} />
-        <InfoPill keyInfo="Following" valueInfo={githubProfile?.following?.toString()!} />
+        <InfoPill
+          keyInfo="Followers"
+          valueInfo={githubProfile?.followers?.toString()!}
+        />
+        <InfoPill
+          keyInfo="Following"
+          valueInfo={githubProfile?.following?.toString()!}
+        />
         <InfoPill keyInfo="Location" valueInfo={githubProfile?.location!} />
       </div>
-      <h1 className="title-page">{githubProfile?.name || githubProfile?.login}</h1>
+      <h1 className="title-page">
+        {githubProfile?.name || githubProfile?.login}
+      </h1>
       <p className="subtitle-page">{githubProfile?.bio}</p>
       <div className="repository-container">
-        {
-          githubRepos.map(repo => (
-            <Repository
-              key={repo.name}
-              repoTitle={repo.name}
-              repoDescription={repo.description || 'No description'}
-              repoLicense={repo.license ? repo.license.name : 'No license'}
-              repoForks={repo.forks_count}
-              repoStars={repo.stargazers_count}
-              repoUpdateInfo={`updated at ${new Date(repo.updated_at).toLocaleDateString()}`}
-            />
-          ))
-        }
+        {githubRepos.map((repo) => (
+          <Repository
+            key={repo.name}
+            repoTitle={repo.name}
+            repoDescription={repo.description || "No description"}
+            repoLicense={repo.license ? repo.license.name : "No license"}
+            repoForks={repo.forks_count}
+            repoStars={repo.stargazers_count}
+            repoUpdateInfo={`updated at ${new Date(
+              repo.updated_at
+            ).toLocaleDateString()}`}
+          />
+        ))}
       </div>
+      {!isShowingAllRepos && (
+        <div className="view-repositories-container">
+          <button type="button" onClick={viewAllRepos}>
+            View all repositories
+          </button>
+        </div>
+      )}
     </main>
   );
 }
